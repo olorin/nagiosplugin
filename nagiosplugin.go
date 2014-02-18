ec66295c5fd50974b73c3d89cc58c4e56b70a257
Sharif Olorin <sio@tesser.org>
2014-02-18 14:00:54 +1100

    Remove example in package comment (it's now in an actual example)

--- nagiosplugin.go
+++ nagiosplugin.go
@@ -1,25 +1,6 @@
 /*
 The nagiosplugin package is a check development framework for Nagios and
 compatible monitoring systems.
-
-The general usage pattern looks like this:
-
-func main() {
-	// Initialize the check - this will return an UNKNOWN result
-	// until more perfdata are added.
-	check := nagiosplugin.NewCheck()
-	// If we exit early or panic() we'll still output a result.
-	defer check.Finish()
-
-	// obtain data here
-
-	// Add an 'OK' result - if no 'worse' check results have been
-	// added, this is the one that will be output.
-	check.AddResult(nagiosplugin.OK, "everything looks shiny, cap'n")
-	// Add some perfdata too (label, unit, min, max, warn, crit).
-	// The math.Inf(1) will be parsed as 'no maximum'. 
-	check.AddPerfDatum("badness", "kb", 0.0, math.Inf(1), 8000.0, 9000.0)
-}
 */
 package nagiosplugin
 
