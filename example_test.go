73dac99ab73479573a236ceb282c55c71cdc020d
Sharif Olorin <sio@tesser.org>
2014-09-05 12:38:17 +1000

    Minor doc fixes

--- example_test.go
+++ example_test.go
@@ -12,7 +12,7 @@ func ExampleExit() {
 func Example() {
 	check := nagiosplugin.NewCheck()
 	// Make sure the check always (as much as possible) exits with
-	// the correct output and return code.
+	// the correct output and return code if we terminate unexpectedly.
 	defer check.Finish()
 	// (If the check panicked on the next line, it'd exit with a
 	// default UNKNOWN result.)
@@ -22,13 +22,13 @@ func Example() {
 	value := math.Pi
 	// We add a dimensionless metric with a minimum of zero, an
 	// unbounded maximum, a warning threshold of 4000.0 and a
-	// critical threshold of 9000.0 (for graphing purposes). 
+	// critical threshold of 9000.0 (for graphing purposes).
 	check.AddPerfDatum("badness", "", value, 0.0, math.Inf(1), 4000.0, 9000.0)
 	// Add an OK check result as the universe appears sane.
 	check.AddResult(nagiosplugin.OK, "Everything looks shiny from here, cap'n")
 	// We potentially perform more checks and add more results here;
 	// if there's more than one, the highest result will be the one
-	// returned (in descending order OK, WARNING, CRITICAL, UNKNOWN).
+	// returned (in ascending order OK, WARNING, CRITICAL, UNKNOWN).
 
 	// Output:
 	// OK: Everything looks shiny from here, cap'n | badness=3.141592653589793;4000;9000;0;
